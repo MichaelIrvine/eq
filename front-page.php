cf6fee7f48507fcf3c96456f093764899e97f6ff
Michael Irvine <michaelirvinedesign@gmail.com>
2021-04-28 16:02:10 -0700

    working out pages/posts

--- front-page.php
+++ front-page.php
@@ -27,17 +27,83 @@ get_header();
 			} ?>
     </div>
   </section>
+  <section id="front-page-content__wrapper">
+    <div id="front-page__col-01" class="col_2x">
+      <div>
+        <?php
+				$featured_cs = get_field('featured_case_study');
+				if ($featured_cs) : ?>
+        <div class="featured-post__wrapper flex__wrapper">
+          <div class="col_2x">
+            <div class="featured-post-title__wrapper">
+              <h4>Case Studies</h4>
+              <h2><?php echo esc_html($featured_cs->post_title); ?></h2>
+            </div>
+            <div class="featured-post-details__wrapper">
+              <h4>Location</h4>
 
-  <?php
-	while (have_posts()) :
-		the_post();
+              <p><?php echo strip_tags($featured_cs->post_content); ?></p>
+              <a href="<?php echo get_the_permalink($featured_cs); ?>">Go To Case Study</a>
+            </div>
+          </div>
+          <div class="col_2x flex__wrapper">
+            <div>
+              <?php echo get_the_post_thumbnail($featured_cs, "full"); ?>
+            </div>
+          </div>
+        </div>
+        <?php endif; ?>
+        <div class="featured-post-image__wrapper">
+          <?php
+					$image = get_field('featured_article_image');
+					$size = 'full';
+					if ($image) {
+						echo wp_get_attachment_image($image, $size);
+					} ?>
+        </div>
+      </div>
+    </div>
+    <div id="front-page__col-02" class="col_2x">
+      <div>
+        <div class="about">
+          <div class="section-title__wrapper">
+            <h4 class="section-title">About</h4>
+          </div>
+          <div>
+            <?php the_field('front_page_about'); ?>
+            <a href="/about" class="eq-link">Read More</a>
+          </div>
+        </div>
+        <div class="news">
+          <div class="featured-post__wrapper">
+            <?php
+						$featured_posts = get_field('featured_news');
+						if ($featured_posts) : ?>
+            <ul>
+              <?php foreach ($featured_posts as $featured_post) :
 
-		get_template_part('template-parts/content', 'page');
+									$post_date = get_the_date("m.d.y", $featured_post->ID);
+									$permalink = get_permalink($featured_post->ID);
+									$title = get_the_title($featured_post->ID);
+									$excerpt = get_the_excerpt($featured_post->ID);
 
-	endwhile; // End of the loop.
-	?>
+								?>
+              <li>
+                <?php echo esc_html($post_date); ?>
+                <?php echo esc_html($excerpt); ?>
+                <a href="<?php echo esc_url($permalink); ?>">Article Link</a>
 
-</main><!-- #main -->
+              </li>
+              <?php endforeach; ?>
+            </ul>
+            <?php endif; ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
 
 <?php
+var_dump($featured_cs);
 get_footer();
--- functions.php
+++ functions.php
@@ -215,9 +215,45 @@ function eq_scripts()
 add_action('wp_enqueue_scripts', 'eq_scripts');
 
 /**
- * Implement the Custom Header feature.
+ * Remove Menu Items from Dashboard
  */
-require get_template_directory() . '/inc/custom-header.php';
+
+function remove_menus()
+{
+	remove_menu_page('edit-comments.php');
+}
+add_action('admin_menu', 'remove_menus');
+
+/**
+ * Change Dashboard Post Title to News
+ */
+function change_post_menu_label()
+{
+	global $menu;
+	global $submenu;
+	$menu[5][0] = 'News';
+	$submenu['edit.php'][5][0] = 'All News';
+	$submenu['edit.php'][10][0] = 'Add News';
+	echo '';
+}
+
+function change_post_object_label()
+{
+	global $wp_post_types;
+	$labels = &$wp_post_types['post']->labels;
+	$labels->name = 'News';
+	$labels->singular_name = 'News';
+	$labels->add_new = 'Add News';
+	$labels->add_new_item = 'Add News';
+	$labels->edit_item = 'Edit News';
+	$labels->new_item = 'News';
+	$labels->view_item = 'View News';
+	$labels->search_items = 'Search News';
+	$labels->not_found = 'No News found';
+	$labels->not_found_in_trash = 'No News found in Trash';
+}
+add_action('init', 'change_post_object_label');
+add_action('admin_menu', 'change_post_menu_label');
 
 /**
  * Custom template tags for this theme.
@@ -240,12 +276,3 @@ require get_template_directory() . '/inc/customizer.php';
 if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
-
-// Remove Menu Items from Dashboard
-
-function remove_menus()
-{
-	remove_menu_page('edit-comments.php');
-}
-add_action('admin_menu', 'remove_menus');
